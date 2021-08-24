054981b6efb5bf99324f180e2d7e4e83f31c1731
Hiroshi Neto <34041982+HiroshiNeto@users.noreply.github.com>
2021-08-24 11:43:14 -0400

    Correção necessária para subir no

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.APP_PORT, '0.0.0.0', function() {
-  console.log('Listening on port ' + process.env.APP_PORT)
+app.listen(process.env.PORT, '0.0.0.0', function() {
+  console.log('Listening on port ' + process.env.PORT)
 });
