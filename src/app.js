675156ff8a0c902056f94e1c801b40002fad5b60
Hiroshi Neto <34041982+HiroshiNeto@users.noreply.github.com>
2021-09-02 10:41:15 -0400

    Adicao de regras de cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ require('dotenv').config();
 require('./database');
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(routes);
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 module.exports = app;
