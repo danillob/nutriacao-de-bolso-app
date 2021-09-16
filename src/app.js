611969c5be14f3f2175d65065eace659bd2bc55e
Hiroshi Neto <34041982+HiroshiNeto@users.noreply.github.com>
2021-09-16 08:25:25 -0400

    Correção de cors

--- src/app.js
+++ src/app.js
@@ -6,15 +6,10 @@ require('dotenv').config();
 require('./database');
 
 const app = express();
-const cors = require('cors');
+var cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
 
 module.exports = app;
