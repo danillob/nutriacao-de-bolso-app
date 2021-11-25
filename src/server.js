61a635ac8140b63924cf570f53ca0060ad90608a
Hiroshi Neto <34041982+HiroshiNeto@users.noreply.github.com>
2021-11-25 00:30:16 -0400

    Correção para subir em prod

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 
-/*app.listen(process.env.PORT, '0.0.0.0', function() {
+app.listen(process.env.PORT, '0.0.0.0', function() {
   console.log('Listening on port ' + process.env.PORT)
-});*/
-
-
-
-app.listen(process.env.APP_PORT, '0.0.0.0', function() {
-  console.log('Listening on port ' + process.env.APP_PORT)
 });
+
+
+
+// app.listen(process.env.APP_PORT, '0.0.0.0', function() {
+//   console.log('Listening on port ' + process.env.APP_PORT)
+// });
