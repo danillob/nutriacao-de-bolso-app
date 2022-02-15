267e8c13466c1c2a271880af50f3548342edf935
Danillo Bittencourt <danillob@gmail.com>
2022-02-15 13:21:22 -0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv').config();
   
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: 'ec2-3-91-128-147.compute-1.amazonaws.com',
+    username: 'wvecavluwpdvqc',
+    password: '5562b4625fbca0d044b389fefc865d00d3aa89fe04c9c2d73195ac79e5efd4ba',
+    database: 'def9p2drcrdtn0',
     dialectOptions: {
         ssl: {
             require: true,
